a5a6b207e34f248215759ef11c9990073fa46386
FreeBz <freebz@hananet.net>
2017-12-16 22:41:24 +0900

    5장 - 품질보증

---
+++ ch05/meadowlark/meadowlark.js
@@ -0,0 +1,57 @@
+var express = require('express');
+
+var fortune = require('./lib/fortune.js');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+
+// 핸들바 뷰 엔진 설정
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+	req.query.test === '1';
+    next();
+});
+
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+app.get('/about', function(req, res){
+    res.render('about', {
+	fortune: fortune.getFortune(),
+	pageTestScript: '/qa/tests-about.js'
+    } );
+});
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
+// 커스텀 404 페이지
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+
+// 커스텀 500 페이지
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+		 app.get('port') + '; press Ctrl-C to terminate.');
+});
