4ff103a9051b56989821aeb2bed1585b2a57261c
FreeBz <freebz@hananet.net>
2018-02-27 14:23:39 +0900

    17장 - 익스프레스에서 MVC 구현

---
+++ ch17/meadowlark/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    bundles: {
+	clientJavaScript: {
+	    main: {
+		file: '/js/meadowlark.min.ae9d1cf6.js',
+		location: 'head',
+		contents: [
+		    '/js/contact.js',
+		    '/js/cart.js',
+		]
+	    }
+	},
+
+	clientCss: {
+	    main: {
+		file: '/css/meadowlark.min.508920e2.css',
+		contents: [
+		    '/css/main.css',
+		    '/css.cart.css',
+		]
+	    }
+	}
+    }
+}
